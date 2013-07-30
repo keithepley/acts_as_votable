b83f264994ff7cd788384eda04c65d6ad10fe590
keithepley <keith.epley@gmail.com>
2013-07-30 11:40:12 -0500

    stabby lambda scopes for rails 4

--- lib/acts_as_votable/vote.rb
+++ lib/acts_as_votable/vote.rb
@@ -15,8 +15,8 @@ module ActsAsVotable
     belongs_to :votable, :polymorphic => true, :touch => true
     belongs_to :voter, :polymorphic => true, :touch => true
 
-    scope :up, where(:vote_flag => true)
-    scope :down, where(:vote_flag => false)
+    scope :up, -> { where vote_flag: true }
+    scope :down, -> { where vote_flag: false }
     scope :for_type, lambda{ |klass| where(:votable_type => klass) }
     scope :by_type,  lambda{ |klass| where(:voter_type => klass) }
 
